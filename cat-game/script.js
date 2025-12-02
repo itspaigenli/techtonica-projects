dca640966684f0ccfd7694ce673caf85fa827c35
Paige Li <itspaige.nli@gmail.com>
2025-12-02 14:35:20 -0800

    JS additions setting positioning of user's cat, and pulling the widths of the game board

--- cat-game/script.js
+++ cat-game/script.js
@@ -1,10 +1,25 @@
 // Variables
 let playerScore = 0;
 let playerLives = 3;
+
 //Display score and lives
 document.getElementById("scoreDisplay").textContent = playerScore;
 document.getElementById("livesDisplay").textContent = playerLives;
-// Move cat with arrow keys
+
+// Access and move cat with arrow keys
+const gameStage = document.getElementById("gameStage");
+const cat = document.getElementById("cat");
+
+// Get widths
+const stageWidth = gameStage.offsetWidth;
+const catWidth = cat.offsetWidth;
+
+// Calculate center position
+const startingLeft = stageWidth / 2 - catWidth / 2;
+
+// Position the cat
+cat.style.left = startingLeft + "px";
+
 // Function to create fish
 // Update game: move fish, check collisions
 // Create fish every 1.5 seconds
