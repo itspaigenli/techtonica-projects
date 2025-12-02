fc88b0fccae19ed71d0522e1e24e0f4359ee3604
Paige Li <itspaige.nli@gmail.com>
2025-12-01 23:23:46 -0800

    Outline for what is needed in the JS file

---
+++ first-game/script.js
@@ -0,0 +1,11 @@
+// Cat stats
+// Past actions
+// Display stats
+// Show history
+// Perform action
+// Apply changes
+// Random event
+// History storage
+// Update stats
+// Helpers
+// Initialize display
