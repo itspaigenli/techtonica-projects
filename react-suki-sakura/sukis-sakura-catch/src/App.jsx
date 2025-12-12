fc6c2db8856f4c77e0b1d762f07f3921a359f7c7
Paige Li <itspaige.nli@gmail.com>
2025-12-12 11:33:47 -0800

    Cleaned files --restart

--- react-suki-sakura/sukis-sakura-catch/src/App.jsx
+++ react-suki-sakura/sukis-sakura-catch/src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from "react";
-import Game from "./components/Game";
-import "./App.css";
-
 export default function App() {
-  // Input state (comes from Controls via callback)
-  const [playerName, setPlayerName] = useState("Player");
-  const [difficulty, setDifficulty] = useState("normal"); // easy | normal | hard
-
   return (
-    <div className="app">
-      <h1>Suki’s Sakura Catch! 🌸🐾</h1>
-
-      <Game
-        playerName={playerName}
-        difficulty={difficulty}
-        onChangePlayerName={setPlayerName}
-        onChangeDifficulty={setDifficulty}
-      />
+    <div>
+      <h1>Suki’s Sakura Catch!</h1>
     </div>
   );
 }
