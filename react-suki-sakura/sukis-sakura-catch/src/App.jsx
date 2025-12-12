ffa883d0961aabd9bf112ff7cd830f6b2c21e002
Paige Li <itspaige.nli@gmail.com>
2025-12-12 11:44:02 -0800

    Add game component inside of assets/components folder

--- react-suki-sakura/sukis-sakura-catch/src/App.jsx
+++ react-suki-sakura/sukis-sakura-catch/src/App.jsx
@@ -1,9 +1,11 @@
-import ".App.css";
+import "./App.css";
+import Game from "./assets/components/game.jsx";
 
 export default function App() {
   return (
     <div className="app">
       <h1>Suki’s Sakura Catch!</h1>
+      <Game />
     </div>
   );
 }
