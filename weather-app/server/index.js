0b55e91e3716c8479ca7377b080e84225dbe28b6
Paige Li <itspaige.nli@gmail.com>
2026-01-21 22:57:09 -0800

    port change to 5050 to fix 403 error

--- weather-app/server/index.js
+++ weather-app/server/index.js
@@ -1,21 +1,29 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config();
+console.log("ENV CHECK:", {
+  LAT: process.env.LAT,
+  LON: process.env.LON,
+  KEY: process.env.OPENWEATHER_API_KEY && "present"
+});
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 5050;
 const { LAT, LON, OPENWEATHER_API_KEY } = process.env;
 
 if (!LAT || !LON || !OPENWEATHER_API_KEY) {
   throw new Error("Missing LAT, LON, or OPENWEATHER_API_KEY in environment variables");
 }
 
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&units=imperial&appid=${OPENWEATHER_API_KEY}`;
-
 app.get("/api/weather", async (req, res) => {
+    console.log("HIT /api/weather");
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&units=imperial&appid=${OPENWEATHER_API_KEY}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
