327052adc6807aea75bba363b9cdfe757b9aa366
Paige Li <itspaige.nli@gmail.com>
2026-01-21 21:02:19 -0800

    Start server

--- weather-app/server/index.js
+++ weather-app/server/index.js
@@ -7,3 +7,8 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+const { LAT, LON, OPENWEATHER_API_KEY } = process.env;
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&units=imperial&appid=${OPENWEATHER_API_KEY}`;
+
+app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
