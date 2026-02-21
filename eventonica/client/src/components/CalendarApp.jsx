087277dd8b071ee756973a2b93897b93bb978dd9
Paige Li <itspaige.nli@gmail.com>
2026-02-21 10:31:05 -0800

    Add boxicons to index.html

--- eventonica/client/src/components/CalendarApp.jsx
+++ eventonica/client/src/components/CalendarApp.jsx
@@ -3,6 +3,9 @@ const CalendarApp = () => {
     <div className="calendar">
       <h1 className="heading">Calendar</h1>
       <div className="navigate-date"></div>
+      <h2 className="month">February</h2>
+      <h2 className="year">2026</h2>
+      <div className="buttons"></div>
       </div>
       </div>
 }
