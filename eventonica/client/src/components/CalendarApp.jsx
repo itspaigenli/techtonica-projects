2b35198333482702ce5bd15881987819566e3860
Paige Li <itspaige.nli@gmail.com>
2026-02-21 10:20:33 -0800

    Add date navigation className to component

--- eventonica/client/src/components/CalendarApp.jsx
+++ eventonica/client/src/components/CalendarApp.jsx
@@ -2,6 +2,7 @@ const CalendarApp = () => {
   return <div className="calendar-app">
     <div className="calendar">
       <h1 className="heading">Calendar</h1>
+      <div className="navigate-date"></div>
       </div>
       </div>
 }
