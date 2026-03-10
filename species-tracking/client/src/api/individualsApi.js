05030fbaf10d8a91c627688094cf973a75ec94d5
Paige Li <itspaige.nli@gmail.com>
2026-03-10 13:15:08 -0700

    Create fetch to create individual

--- species-tracking/client/src/api/individualsApi.js
+++ species-tracking/client/src/api/individualsApi.js
@@ -7,3 +7,19 @@ export async function getIndividuals() {
 
   return response.json();
 }
+
+export async function createIndividual(newIndividual) {
+  const response = await fetch("/individuals", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newIndividual),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to create individual");
+  }
+
+  return response.json();
+}
