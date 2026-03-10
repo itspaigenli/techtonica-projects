a1395888d0e743313b5bcb6a6dfe19ca8b95ffa0
Paige Li <itspaige.nli@gmail.com>
2026-03-10 12:49:39 -0700

    Create api fetch for creating sightings

--- species-tracking/client/src/api/sightingsApi.js
+++ species-tracking/client/src/api/sightingsApi.js
@@ -7,3 +7,19 @@ export async function getSightings() {
 
   return response.json();
 }
+
+export async function createSighting(newSighting) {
+  const response = await fetch("/sightings", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newSighting),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to create sighting");
+  }
+
+  return response.json();
+}
