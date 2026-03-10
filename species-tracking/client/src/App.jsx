640199ff6e7cf318d938a47b2901efc741231316
Paige Li <itspaige.nli@gmail.com>
2026-03-10 13:06:41 -0700

    Add useState and counter update to assist with auto refresh

--- species-tracking/client/src/App.jsx
+++ species-tracking/client/src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 import SightingsList from "./components/SightingsList";
 import SpeciesList from "./components/SpeciesList";
 import IndividualsList from "./components/IndividualsList";
+import SightingsForm from "./components/SightingsForm";
 
 function App() {
+  const [refreshKey, setRefreshKey] = useState(0);
+
+  function handleSightingAdded() {
+    setRefreshKey((prev) => prev + 1);
+  }
+
   return (
     <main>
       <h1>FaunaDex</h1>
       <p>Animal Sighting Tracker</p>
 
-      <SightingsList />
+      <SightingsForm onSightingAdded={handleSightingAdded} />
+      <SightingsList refreshKey={refreshKey} />
       <SpeciesList />
       <IndividualsList />
     </main>
