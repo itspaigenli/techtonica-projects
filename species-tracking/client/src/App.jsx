f0b3d8e32f74da9575b8993941a4a041322cd413
Paige Li <itspaige.nli@gmail.com>
2026-03-10 14:57:01 -0700

    Update to App.jsx to include new import routes

--- species-tracking/client/src/App.jsx
+++ species-tracking/client/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import SightingsList from "./components/SightingsList";
 import SpeciesList from "./components/SpeciesList";
 import IndividualsList from "./components/IndividualsList";
 import SightingsForm from "./components/SightingsForm";
 import IndividualsForm from "./components/IndividualsForm";
+import IndividualDetail from "./components/IndividualDetail";
 
 function App() {
   const [refreshKey, setRefreshKey] = useState(0);
@@ -14,17 +16,26 @@ function App() {
   }
 
   return (
-    <main>
-      <h1>FaunaDex</h1>
-      <p>Animal Sighting Tracker</p>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <main>
+            <h1>FaunaDex</h1>
+            <p>Animal Sighting Tracker</p>
 
-      <SightingsForm onSightingAdded={handleDataChange} />
-      <IndividualsForm onIndividualAdded={handleDataChange} />
+            <SightingsForm onSightingAdded={handleDataChange} />
+            <IndividualsForm onIndividualAdded={handleDataChange} />
 
-      <SightingsList refreshKey={refreshKey} />
-      <IndividualsList refreshKey={refreshKey} />
-      <SpeciesList />
-    </main>
+            <SightingsList refreshKey={refreshKey} />
+            <IndividualsList refreshKey={refreshKey} />
+            <SpeciesList />
+          </main>
+        }
+      />
+
+      <Route path="/individuals/:id" element={<IndividualDetail />} />
+    </Routes>
   );
 }
 
