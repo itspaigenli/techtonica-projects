9b4f21100279ba84a665e1d7b56ef814e42e9dab
Paige Li <itspaige.nli@gmail.com>
2026-03-10 12:19:11 -0700

    Add import and shortcut to App.jsx for species list

--- species-tracking/client/src/App.jsx
+++ species-tracking/client/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import SightingsList from "./components/SightingsList";
+import SpeciesList from "./components/SpeciesList";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <p>Animal Sighting Tracker</p>
 
       <SightingsList />
+      <SpeciesList />
     </main>
   );
 }
