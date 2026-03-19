7847486b46480507fbfd92c9b893955c3ba8c3e9
Paige Li <itspaige.nli@gmail.com>
2026-03-19 15:19:25 -0700

    Input router.post template

--- contact-list/server/src/routes/contacts.js
+++ contact-list/server/src/routes/contacts.js
@@ -3,6 +3,7 @@ import pool from "../db.js";
 
 const router = express.Router();
 
+// Purpose: Fetch all contacts from the database and send them to the client (http://localhost:3000/contacts)
 router.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM contacts ORDER BY id;");
@@ -10,6 +11,25 @@ router.get("/", async (req, res) => {
   } catch (error) {
     console.error("GET /contacts error:", error);
     res.status(500).json({ error: "Failed to fetch contacts." });
+  }
+});
+
+// POST NEW -
+router.post("/", async (req, res) => {
+  try {
+    const { field1, field2 } = req.body;
+
+    const result = await pool.query(
+      `INSERT INTO table_name (field1, field2)
+      VALUES ($1, $2)
+      RETURNING *`,
+      [field1, field2],
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    console.error("POST /table_name error:", error);
+    res.status(500).json({ error: "Server error creating record." });
   }
 });
 
