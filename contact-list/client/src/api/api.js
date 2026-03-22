020f217a4fd413720f12bf55c4bd47857fb8ec96
Paige Li <itspaige.nli@gmail.com>
2026-03-22 08:30:53 -0700

    Update template to include api url and replaced syntax with correct keywords

--- contact-list/client/src/api/api.js
+++ contact-list/client/src/api/api.js
@@ -1,35 +1,39 @@
-export async function getItems() {
-  const response = await fetch(`/items`);
+const API_URL = import.meta.env.VITE_API_URL;
+
+export async function getContacts() {
+  const response = await fetch(`${API_URL}/contacts`);
 
   if (!response.ok) {
-    throw new Error("Failed to fetch items");
+    throw new Error("Failed to fetch contacts");
   }
 
   return response.json();
 }
 
-export async function createItem(item) {
-  const response = await fetch(`/items`, {
+export async function createContact(contact) {
+  const response = await fetch(`${API_URL}/contacts`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(item),
+    body: JSON.stringify(contact),
   });
 
   if (!response.ok) {
-    throw new Error("Failed to create item");
+    throw new Error("Failed to create contact");
   }
 
   return response.json();
 }
 
-export async function deleteItem(id) {
-  const response = await fetch(`/items/${id}`, {
+/* for later
+export async function deleteContact(id) {
+  const response = await fetch(`${API_URL}/contacts/${id}`, {
     method: "DELETE",
   });
 
   if (!response.ok) {
-    throw new Error("Failed to delete item");
+    throw new Error("Failed to delete contact");
   }
-}
+} 
+  */
