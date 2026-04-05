d65233859d907114ef34d564d38210d9d2478ee9
Paige Li <itspaige.nli@gmail.com>
2026-04-04 21:01:02 -0700

    Create GET router to db

--- blog-app/server/src/routes/posts.js
+++ blog-app/server/src/routes/posts.js
@@ -1,10 +1,36 @@
 import express from "express";
+import pool from "../db.js";
 
 const router = express.Router();
 
-// GET /posts
+// GET /posts (actual data)
+router.get("/", async (req, res) => {
+  try {
+    const sql = `
+      SELECT
+        p.id,
+        p.title,
+        p.content,
+        p.status,
+        p.publish_date,
+        p.feature_image_url,
+        c.name AS category_name
+      FROM posts p
+      LEFT JOIN categories c ON p.category_id = c.id
+      ORDER BY p.id;
+    `;
+
+    const result = await pool.query(sql);
+    res.json(result.rows);
+  } catch (err) {
+    console.error("GET /posts error:", err);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+/* GET /posts (test connection)
 router.get("/", (req, res) => {
   res.json([{ message: "GET /posts working" }]);
-});
+}); */
 
 export default router;
